a8bef126329806660d3b2c84b687afc8cf918639
BinError <marcknmt@gmail.com>
2022-02-11 17:54:04 -0300

    ejercicio 5 hecho

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -34,6 +34,8 @@ function multiplicarNumeroDesconocidoPorCinco(objetoMisterioso) {
   // "objetoMisterioso" tiene una propiedad llamada "numeroMisterioso"
   // Multiplica el numeroMisterioso por 5 y devuelve el producto
   // Tu código:
+  objetoMisterioso.numeroMisterioso *= 5;
+  return objetoMisterioso.numeroMisterioso;
 
 }
 
